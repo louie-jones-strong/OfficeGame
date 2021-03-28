4cff6fe200987d9b66dc20335c21572323b76991
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2021-03-28 21:41:43 +0100

    try to fkip screens in web gl

---
+++ Unity/Office/Assets/Scripts/ScreenFlip.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFlip : MonoBehaviour
+{
+    void Awake()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        var scale = transform.localScale;
+        scale.y *= -1;
+        transform.localScale = scale;
+#endif
+    }
+}
